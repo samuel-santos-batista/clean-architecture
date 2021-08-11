8a714779302fa30ae2026f9fdcfaf95782647ae7
Samuel <samuelsantxxi@gmail.com>
2021-08-11 00:43:43 -0300

    feat: ensure cors is anable

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,6 +1,8 @@
 import { Express } from 'express'
 import { bodyParser } from '../middlawares/body-parser'
+import { cors } from '../middlawares/cors'
 
 export default (app: Express): void => {
   app.use(bodyParser)
+  app.use(cors)
 }
