37fb6daacc40dfc4792fda29058902bec81c5d44
Samuel <samuelsantxxi@gmail.com>
2021-08-12 01:18:00 -0300

    refactor: connect to mongo before integration tests

---
+++ src/main/adapters/express-routes-adapter.ts
@@ -0,0 +1,13 @@
+import { Controller, HttpRequest } from "../../presentation/protocols";
+import { Request, Response } from 'express'
+
+export const adaptRoute = (controller: Controller) => {
+  return  async (request: Request, response: Response) => {
+    const httpRequest: HttpRequest = {
+      body: request.body
+    }
+    console.log(request)
+    const httpResponse = await controller.handle(httpRequest)
+    response.status(httpResponse.statusCode).json(httpResponse.body)
+  }
+}
--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -1,9 +1,21 @@
 import request from 'supertest'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 import app from '../config/app'
 
 
 describe('SignUp Routes', () => {
-  test('Should return an accauntt on success', async () => {
+  beforeAll(async ()=>{
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+  afterAll(async ()=>{
+    await MongoHelper.disconnect()
+  })
+  beforeAll(async ()=>{
+    const accountCollection = MongoHelper.getColletion('accounts')
+    await accountCollection.deleteMany({})
+  })
+
+  test('Should return an account on success', async () => {
     await request(app)
       .post('/api/signup')
       .send({
